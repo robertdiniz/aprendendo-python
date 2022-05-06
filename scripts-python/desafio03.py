b05a2f3ed0231d624bd87de38e3dcff2b406af1d
Robert Diniz <94693689+robertdiniz@users.noreply.github.com>
2022-05-06 17:19:18 -0300

    Add files via upload

---
+++ scripts-python/desafio03.py
@@ -0,0 +1,5 @@
+number1 = input('Insira um número: ')
+number2 = input('Insira outro número: ')
+soma = number1 + number2
+print(soma)
+print('A soma dos números é: ' , number1 + number2)
---
+++ scripts-python/teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual é seu peso? ')
+print(nome, idade, peso)
