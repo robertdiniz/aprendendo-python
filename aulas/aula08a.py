f4be4f6c026ec7399726d76ae64b78716dd1832f
Robert Diniz <94693689+robertdiniz@users.noreply.github.com>
2022-05-10 17:32:44 -0300

    Add files via upload

---
+++ aulas/aula08a.py
@@ -0,0 +1,3 @@
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+print(f'A raíz de {num} é igual a {floor(sqrt(num)):.2f}')
---
+++ aulas/aula08b.py
@@ -0,0 +1,5 @@
+import random
+num = random.random()
+print('Número aleatório: ', num)
+num = random.randint(1, 10)
+print('Número inteiro: ', num)
